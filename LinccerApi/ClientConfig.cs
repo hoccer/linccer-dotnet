cc96bfddab3f169d80e6c6418b76cbc05431eef9
Mamta Bansal <mamta@hoccer.com>
2011-03-21 11:01:02 +0100

    renaming of Api project

--- LinccerApi/ClientConfig.cs
+++ LinccerApi/ClientConfig.cs
@@ -12,14 +12,23 @@ namespace LinccerApi
         {
             UseBetaServers ();
         }
-        
+
         public string ClientUri {
             get { return LinccerUri + "/clients/" + ClientId; }
         }
+
         public string LinccerUri { get; set; }
+
         public string ClientId {
             get { return "1122ffaa-8c99-49ee-b045-33d737cc50f9"; }
         }
+
+        public string ApiKey {
+            get {
+               return "e101e890ea97012d6b6f00163e001ab0";
+            }
+        }
+
         public void UseBetaServers ()
         {
             LinccerUri = "http://linccer-beta.hoccer.com/v3";
---
+++ LinccerApi/Linccer.cs
@@ -0,0 +1,87 @@
+
+using System;
+using System.Net;
+
+
+namespace LinccerApi
+{
+
+
+	public class Linccer
+	{
+
+		public Linccer (String name)
+		{
+			this.Name = name;
+			Environment = new Environment ();
+		}
+
+		public String Name { get; set; }
+		public LinccerApi.Environment Environment { get; set; }
+
+		public ClientConfig Config { get; set; }
+
+
+		public void OnGpsChanged (Double lat, Double lon, int acc)
+		{
+			Console.WriteLine ("OnGpsChanged");
+			
+			TimeSpan ts = DateTime.UtcNow - new DateTime (1970, 1, 1, 0, 0, 0);
+			
+			Environment.gps = new LocationInfo { latitude = lat, longitude = lon, accuracy = acc, timestamp = (int)ts.TotalSeconds };
+			
+			using (var client = new WebClient ()) {
+				
+				System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding ();
+				string uri = Config.ClientUri + "/environment";
+				client.UploadData (Sign (uri), "PUT", enc.GetBytes (Environment.ToString ()));
+			}
+		}
+
+		public void Share (String mode)
+		{
+			Console.WriteLine ("share");
+		}
+
+		public void Share (String mode, Object payload)
+		{
+			System.Web.Script.Serialization.JavaScriptSerializer oSerializer = new System.Web.Script.Serialization.JavaScriptSerializer ();
+			string sJSON = oSerializer.Serialize (payload);
+			
+			Console.WriteLine (sJSON);
+			using (var client = new WebClient ()) {
+				
+				System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding ();
+				string uri = Config.ClientUri + "/action/one-to-many";
+				client.UploadData (Sign (uri), "PUT", enc.GetBytes (sJSON));
+			}
+		}
+
+		public T Receive<T> () where T : new()
+		{
+			
+			
+			using (var client = new WebClient ()) {
+				
+				
+				string uri = Config.ClientUri + "/action/one-to-many";
+				string json = client.DownloadString (Sign (uri));
+				if (json == null)
+					return default(T);
+				
+				System.Web.Script.Serialization.JavaScriptSerializer oSerializer = new System.Web.Script.Serialization.JavaScriptSerializer ();
+				
+				Console.WriteLine ("Received: " + json);
+				return oSerializer.Deserialize<T[]> (json)[0];
+			}
+			
+			
+		}
+
+		private string Sign (string uri)
+		{
+			uri += "?api_key=Config.ApiKey";
+			return uri;
+		}
+	}
+}
