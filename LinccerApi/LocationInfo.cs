ace67e30316fded780d52b6ec8a7a0f621927e61
Mamta Bansal <mamta@hoccer.com>
2011-03-30 12:28:14 +0200

    adding of License

--- LinccerApi/LocationInfo.cs
+++ LinccerApi/LocationInfo.cs
@@ -1,3 +1,31 @@
+/*
+Copyright (C) 2009, 2010, Hoccer GmbH Berlin, Germany <www.hoccer.com>
+
+These coded instructions, statements, and computer programs contain
+proprietary information of Linccer GmbH Berlin, and are copy protected
+by law. They may be used, modified and redistributed under the terms
+of GNU General Public License referenced below.
+
+Alternative licensing without the obligations of the GPL is
+available upon request.
+
+GPL v3 Licensing:
+
+This file is part of the "Linccer .Net-API".
+
+Linccer .Net-API is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Linccer .Net-API is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Linccer .Net-API. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 using System;
 using System.Runtime.Serialization;
@@ -6,7 +34,7 @@ namespace LinccerApi
 {
 
 
-[DataContract]
+
     public class LocationInfo
     {
 
@@ -15,15 +43,15 @@ namespace LinccerApi
             Timestamp = Utils.TimeNow;
         }
 
-        [DataMember(Name = "latitude")]
-        public double Latitude { get; set; }
-        [DataMember(Name = "longitude")]
-        public double Longitude { get; set; }
-        [DataMember(Name = "accuracy")]
-        public int Accuracy { get; set; }
-        [DataMember(Name = "timestamp")]
-        public int Timestamp { get; set; }
 
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public int Accuracy { get; set; }
+
+        public int Timestamp { get; set; }
+        
         
         
     }
