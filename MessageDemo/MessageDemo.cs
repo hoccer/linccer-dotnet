55039881bcdedca44f69eb107570bf7a5a948c8b
Rodja Trappe <rodja@hoccer.com>
2011-03-31 14:44:44 +0200

    fixed some README content

--- MessageDemo/MessageDemo.cs
+++ MessageDemo/MessageDemo.cs
@@ -7,12 +7,7 @@ namespace MessageDemo
 
     class Data
     {
-        public Data ()
-        {
-        }
-
         public string Message { get; set; }
-        
     }
 
     class MessageDemo
@@ -22,19 +17,28 @@ namespace MessageDemo
             Linccer linccer = new Linccer ();
             linccer.Config = new ClientConfig ("C# Message Demo");
             linccer.Config.UseSandboxServers ();
+
+            // set geo information and send it to the server
             linccer.Gps = new LocationInfo { Latitude = 52.5157, Longitude = 13.409, Accuracy = 1000 };
             linccer.SubmitEnvironment ();
             
             
             if (args.Length > 0) {
+
+                // countdown
                 for (int i = 3; i > 0; i--) {
                     System.Console.Write (i + "... ");
                     Thread.Sleep (1 * 1000);
                 }
+
                 linccer.Share ("one-to-many", new Data { Message = args[0] });
+
             } else {
+
                 System.Console.WriteLine ("Waiting for message");
                 Data receivedMessage;
+
+                // constantly try to receive a message
                 do {
                     receivedMessage = linccer.Receive<Data> ("one-to-many", "waiting=true");
                 } while (receivedMessage == null);
